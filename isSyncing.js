dd9d9d77481161b8c106b71de4d36ed8987c7875
lifo1994 <w369gf523@gmail.com>
2017-12-14 22:41:00 -0800

    improve event

--- isSyncing.js
+++ isSyncing.js
@@ -6,9 +6,9 @@ const web3 = new Web3(new Web3.providers.WebsocketProvider('http://127.0.0.1:123
 var subscription = web3.eth.subscribe('newBlockHeaders');
 //"data" : Fires on each incoming block header.
 subscription.on("data", function(blockHeader){
-	console.log("found block : " + blockHeader.number);
+	//console.log("found block : " + blockHeader.number);
 	//console.log(blockHeader);
-	Revolver.block_high = blockHeader.number;
+	my_revolver.emit("load", blockHeader.number);
 });
 
 
@@ -22,33 +22,30 @@ class Revolver extends EventEmitter {
 
 		//events
 		this.on("consume", function(){
-			if(this.block_low < this.block_high){
-				//trigger event here
-				
-				this.block_low++;
-				console.log("consume block");
-			}
-			else{
-				console.log("consume fail");
+			while(true){
+				if(this.block_low < this.block_high){
+					//trigger event here
+					
+					this.block_low++;
+					console.log("consume block");
+					continue;
+				}
+				else{
+					console.log("consume fail");
+					break;
+				}
 			}
 		});
 
 		this.on("load", function(high){
 			this.block_high = high;
-		});
-
-		
+		});	
 	}
 }
 
 
 my_revolver = new Revolver();
+var interval = setInterval(()=>{console.log(my_revolver)}, 3000);
 
 
-var interval = setInterval(()=>{console.log(my_revolver)}, 1000);
-
-
-my_revolver.emit("load", 2000);
-
-// my_revolver.emit("load");
-// my_revolver.emit("load");
+var interval2 = setInterval(()=>{my_revolver.emit("consume");}, 1000);
