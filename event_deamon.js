c88372684ee74ef4bcf943e8b3d5a76043f03efe
lifo1994 <w369gf523@gmail.com>
2017-12-19 23:29:18 -0800

    save

--- event_deamon.js
+++ event_deamon.js
@@ -4,86 +4,104 @@ var set = require('./intersection.js');
 
 const web3 = new Web3(new Web3.providers.WebsocketProvider('http://127.0.0.1:1235'))
 
-function Listen_contract(filename){
-	//private var
-	var _contract_list = {};
+//here should ignore abi here, maybe
+function Listen_contract(filename) {
+    //private var
+    var _contract_list = {};
 
-	//constructor
-	try{
-		//to lower case, address is Hex A = a
-		_contract_list = JSON.parse( fs.readFileSync(filename, 'utf8').toLowerCase() );
-	}catch(err){
-		console.log("Can't find file : " + filename);
-	}
+    //constructor
+    try {
+        //to lower case, address is Hex A = a
+        _contract_list = JSON.parse(fs.readFileSync(filename, 'utf8').toLowerCase());
+    } catch (err) {
+        console.log("Can't find file : " + filename);
+    }
 
-	//public 
-	return {
-		show : function(){
-			console.log("-------listen_contract.show()-------");
-			//console.log(_contract_list);
-			console.log(JSON.stringify(_contract_list, null, "    "));
-			console.log("-----listen_contract.show() END-----");
-		},
-		
-		abi : function(search_address){
-			return _contract_list[search_address];
-		},
+    //public 
+    return {
+        show: function() {
+            console.log("-------listen_contract.show()-------");
+            //console.log(_contract_list);
+            console.log(JSON.stringify(_contract_list, null, "    "));
+            console.log("-----listen_contract.show() END-----");
+        },
 
-		address_list : Object.keys(_contract_list)
-	}
+        abi: function(search_address) {
+            return _contract_list[search_address];
+        },
+
+        address_list: Object.keys(_contract_list)
+    }
 }
 
-function instersection_contracts(_tar_contracts, meta_contracts){
-	return set.intersect(_tar_contracts, meta_contracts);
+function instersection_contracts(_tar_contracts, meta_contracts) {
+    return set.intersect(_tar_contracts, meta_contracts);
 }
 
-async function related_contract_address_list(transactions){
-	var contract_list = [];
-	////*****change to parallel version
-	for(transaction of transactions){
+//this should be ignore
+// async function related_contract_address_list(transactions) {
+//     var contract_list = [];
+//     ////*****change to parallel version
+//     for (transaction of transactions) {
 
-		var receipt = await web3.eth.getTransactionReceipt(transaction);
-		//the contract address is what user interact to.
-		contract_list.push(receipt.to);
-		//console.log(receipt);
-	}
-	return contract_list;
+//         var receipt = await web3.eth.getTransactionReceipt(transaction);
+//         //the contract address is what user interact to.
+//         contract_list.push(receipt.to);
+//         //console.log(receipt);
+//     }
+//     return contract_list;
+// }
+
+async function related_contract(block_num) {
+    let block_info = await web3.eth.getBlock(block_num);
+    let transactions = block_info.transactions;
+    var contract_list = [];
+    ////*****change to parallel version
+    for (transaction of transactions) {
+        var receipt = await web3.eth.getTransactionReceipt(transaction);
+        //the contract address is what user interact to.
+        contract_list.push(receipt.to);
+        //console.log(receipt);
+    }
+    return contract_list;
 }
 
-function Block_surveillance(address_list){
-	var _block_num_last = -1;
-	var _tar_contracts = address_list;
 
-	async function listen(){
-		let checking_block_num = await web3.eth.getBlockNumber();
 
-		if(_block_num_last != checking_block_num){
-			_block_num_last = checking_block_num;
-			console.log("block : " + checking_block_num);
+function Block_surveillance(address_list) {
+    var _block_num_last = -1;
+    var _tar_contracts = address_list;
 
-			let block_info = await web3.eth.getBlock(checking_block_num);
-			var trigger_contract = await related_contract_address_list(block_info.transactions);
+    async function listen() {
+        let checking_block_num = await web3.eth.getBlockNumber();
 
-			return trigger_contract;
-			// var instersection1 = instersection_contracts(_tar_contracts, trigger_contract);
+        if (_block_num_last != checking_block_num) {
+            _block_num_last = checking_block_num;
+            console.log("block : " + checking_block_num);
 
-			// //console.log(instersection1);
-			// if( instersection1.length > 0 ){
-			//  	console.log(instersection1);
-			//  	console.log('event!!');
-			// }
-		}
-	}
+            let block_info = await web3.eth.getBlock(checking_block_num);
+            var trigger_contract = await related_contract_address_list(block_info.transactions);
 
-	return{
-		//***change***block from
-		listen_all : async function(){
-			return await listen();
-		},
-		listen_own : async function(){
-			return instersection_contracts(_tar_contracts, await listen());
-		}
-	}
+            return trigger_contract;
+            // var instersection1 = instersection_contracts(_tar_contracts, trigger_contract);
+
+            // //console.log(instersection1);
+            // if( instersection1.length > 0 ){
+            //  	console.log(instersection1);
+            //  	console.log('event!!');
+            // }
+        }
+    }
+
+    return {
+        //***change***block from
+        listen_all: async function() {
+            return await listen();
+        },
+        listen_own: async function() {
+            return instersection_contracts(_tar_contracts, await listen());
+        }
+    }
 }
 
 
@@ -94,12 +112,15 @@ function Block_surveillance(address_list){
 // var interval = setInterval(transactions, 1000);
 
 
-my_listen_contract = Listen_contract("file");
-surveillance = Block_surveillance(my_listen_contract.address_list);
+// my_listen_contract = Listen_contract("file");
+// surveillance = Block_surveillance(my_listen_contract.address_list);
 
-var interval = setInterval(surveillance.listen_own, 1000);
+// var interval = setInterval(surveillance.listen_own, 1000);
 
 
 //transactions();
 //transactions("file");
 
+related_contract(1000000).then(
+    (value) => console.log(value)
+);
